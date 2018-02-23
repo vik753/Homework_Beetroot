a131c1f86d3f4af5256b2224cd4069d8eac51394
Igor Korenets <vik753@gmail.com>
2018-02-23 12:00:13 +0200

    js-test-timer add month calculating

--- js/index.js
+++ js/index.js
@@ -3,21 +3,42 @@ let inputDays = document.getElementById('input-days'),
     inputMinutes = document.getElementById('input-minutes'),
     inputSeconds = document.getElementById('input-seconds'),
     datetimeLocal = document.getElementById('dateInput'),
+    monthInfo = document.getElementById('monthInfo'),
     btnStart = document.getElementById('btn-start'),
     btnStop = document.getElementById('btn-stop');
 
-    inputDays.disabled = true;
-    inputHours.disabled = true;
-    inputMinutes.disabled = true;
-    inputSeconds.disabled = true;
+inputDays.disabled = true;
+inputHours.disabled = true;
+inputMinutes.disabled = true;
+inputSeconds.disabled = true;
 
 
 let countdownTimer,
+    userDateTime,
     current_level;
 
 const norm = (val) => val < 10 ? '0' + val : val;
 
+function getLastDayInMonth(year, month) {
+    let date = new Date(year, month + 1, 0);
+    return date.getDate();
+}
+
 function timer() {
+
+    let now = new Date();
+    let daysInThisMonth = (getLastDayInMonth(now.getFullYear(), now.getMonth()) - now.getDate());//возвращает оставшееся количество дней в этом месяце
+
+    if (userDateTime.getMonth() - now.getMonth() < 2 && (userDateTime.getMonth() - now.getMonth()) > 0) {
+        monthInfo.innerHTML = (`In this month ${daysInThisMonth} days left, and in next month ${userDateTime.getDate()} days left.`);
+    } else if (userDateTime.getMonth() - now.getMonth() >= 2) {
+        let lastFullMonths = 0;
+        for (let x = now.getMonth() + 1; x < userDateTime.getMonth(); x++) {
+            lastFullMonths++;
+        }
+        monthInfo.innerHTML = (`In this month ${daysInThisMonth} days left, and<br>The ${lastFullMonths} full months left, and plus<br>In last month ${userDateTime.getDate()} days left.`);
+    }
+
 
     let days = Math.floor(current_level / 86400);
     let remainsDays = current_level - (days * 86400);//остаток секунд за вычетом дней
@@ -37,7 +58,7 @@ function timer() {
     inputSeconds.value = norm(seconds);
 
 
-    if(current_level <= 0){
+    if (current_level <= 0) {
         clearInterval(countdownTimer);
         current_level = 0;
         btnStart.disabled = false;
@@ -50,10 +71,10 @@ function timer() {
     current_level--;
 }
 
-btnStart.onclick = function(){
-    let currentDateTime = new Date(),
-        userDateTime = new Date(datetimeLocal.value);
-    if((!userDateTime.isNaN) && (userDateTime - currentDateTime) > 0){
+btnStart.onclick = function () {
+    let currentDateTime = new Date();
+    userDateTime = new Date(datetimeLocal.value);
+    if ((!userDateTime.isNaN) && (userDateTime - currentDateTime) > 0) {
         current_level = Math.floor((userDateTime - currentDateTime) / 1000);
         btnStart.disabled = true;
         countdownTimer = setInterval(timer, 1000);
@@ -64,7 +85,7 @@ btnStart.onclick = function(){
     }
 }
 
-btnStop.onclick = function(){
+btnStop.onclick = function () {
     btnStart.disabled = false;
     current_level = 0;
     clearInterval(countdownTimer);
