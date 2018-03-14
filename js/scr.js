37f05fc8ed0c46e5d132acf89be8c323a0ee1aa1
Igor Korenets <vik753@gmail.com>
2018-03-14 12:49:59 +0200

    set autoslide

--- js/scr.js
+++ js/scr.js
@@ -4,31 +4,51 @@ $(function () {
 		, btn = $('.btn')
 		, slider = $('.slider')
 		, containerSlider = $('.container-slider');
+	let intervalId;
 	
 	containerSlider.css('overflow', 'hidden');
 	slider.find('li').stop(true, true).fadeOut();//http://api.jquery.com/stop/
 	slider.find('li:first').stop(true, true).fadeIn();
 	
+	autoSlide();
 	
-	btnRight.on('click', function () {
+	function autoSlide() {
+		clearInterval(intervalId);
+		intervalId = setInterval(toLeft, 5000);
+	}
+	
+	
+	btnRight.on('click', () => {
 		event.preventDefault();
-		slider.find('li:first').stop(true, true).fadeOut(700);
+		toRight();
+		clearInterval(intervalId);
+		intervalId = setInterval(toRight, 5000);
+	});
+	
+	btnLeft.on('click', () => {
+		event.preventDefault();
+		toLeft();
+		clearInterval(intervalId);
+		intervalId = setInterval(toLeft, 5000);
+	});
+	
+	
+	function toLeft() {
+		slider.find('li:first').stop(true, true).fadeOut(500);
+		let lastImg = slider.find('li:last').clone();
+		slider.find('li:last').remove();
+		slider.prepend(lastImg);
+		slider.find('li:first').stop(true, true).fadeIn(500);
+	}
+	
+	function toRight() {
+		slider.find('li:first').stop(true, true).fadeOut(500);
 		let firstImg = slider.find('li:first').clone();
 		slider.find('li:first').remove();
 		slider.append(firstImg);
 		slider.find('li:last').stop(true, true).fadeOut();
-		slider.find('li:first').stop(true, true).fadeIn(700);
-		
-		
-	});
-	
-	btnLeft.on('click', function () {
-		event.preventDefault();
-		let lastImg = slider.find('li:last').clone();
-		slider.find('li:last').remove();
-		slider.find('li:first').stop(true, true).fadeOut(700);
-		slider.prepend(lastImg);
-		slider.find('li:first').stop(true, true).fadeIn(700);
-	});
+		slider.find('li:first').stop(true, true).fadeIn(500);
+	}
 	
 });
+
